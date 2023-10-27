928d5c57dfa39972b9842151e314315967ae93e8
WeCanStart <113310973+WeCanStart@users.noreply.github.com>
2023-10-27 21:04:31 +0300

    Update

--- app.cpp
+++ app.cpp
@@ -1,5 +1,7 @@
+#include <fstream>
 #include <vector>
 #include <string>
+#include <filesystem>
 
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
@@ -99,12 +101,14 @@ int main(int, char**)
     //fileDialog.SetTypeFilters({ ".gitignore" });
     fileDialog.SetTitle("Select path to repo");
     std::string workPath = "";
+    std::string readPath = "";
 
     bool open = 0;
+    ImFont* previousFont = ImGui::GetFont();
 
     TextEditor editor;
     auto lang = TextEditor::LanguageDefinition::CPlusPlus();
-
+    editor.SetLanguageDefinition(lang);
 
 #ifdef __EMSCRIPTEN__
     io.IniFilename = nullptr;
@@ -124,25 +128,21 @@ int main(int, char**)
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
+        int x, y;
+        glfwGetWindowSize(window, &x, &y);
+        //glfwSetFont();
         {
-            int x, y;
-            glfwGetWindowSize(window, &x, &y);
-            //glfwSetFont();
-
             ImGui::SetNextWindowPos(ImVec2(0, 0), 0, ImVec2(0, 0));
             ImGui::SetNextWindowSize(ImVec2((float)(x) / 2, y), 0);
 
-
             ImGui::Begin("Text");
+            ImGui::Text(std::filesystem::current_path().string().data());
             ImGui::SetWindowFontScale((float)(x) / 1920);
             ImGui::Text("artem");
             ImGui::End();
         }
 
         {
-            static float f = 0.0f;
-            int x, y;
-            glfwGetWindowSize(window, &x, &y);
             //glfwSetFont();
 
             ImGui::SetNextWindowPos(ImVec2((float)(x) / 2, 0), 0, ImVec2(0, 0));
@@ -180,14 +180,31 @@ int main(int, char**)
                 }
             }
             ImGui::SameLine();
+            bool op = false;
             if (ImGui::Button("Set path to repo")) {
+                op = true;
                 fileDialog.Open();
+                //fileDialog.
             }
+            fileDialog.Display();
+            
+            if (workPath != readPath) {
+                readPath = workPath;
+                std::ifstream t(readPath + std::string("\\1691C.cpp"), std::ios_base::in | std::ios_base::out);
+                if (t.good())
+                {
+                    std::string str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+                    editor.SetText(str);
+                }
+            }
+            
+            //fileDialog.Close();
             ImGui::End();
 
             if (open) {
                 ImGui::SetNextWindowSize(ImVec2(200, 200));
                 ImGui::Begin("Didn't set path for review yet");
+                ImGui::SetWindowFontScale((float)(x) / 1920);
                 if (ImGui::Button("Close")) {
                     open = false;
                 }
@@ -195,11 +212,79 @@ int main(int, char**)
             }
         }
 
-        fileDialog.Display();
+        auto cpos = editor.GetCursorPosition();
+
+        ImGui::Begin("Text Editor Demo", nullptr, ImGuiWindowFlags_HorizontalScrollbar | ImGuiWindowFlags_MenuBar);
+        ImGui::SetWindowFontScale((float)(x) / 1920);
+        ImGui::SetWindowSize(ImVec2(800, 600), ImGuiCond_FirstUseEver);
+        if (ImGui::BeginMenuBar())
+        {
+            if (ImGui::BeginMenu("File"))
+            {
+                if (ImGui::MenuItem("Save"))
+                {
+                    auto textToSave = editor.GetText();
+                    /// save text....
+                }
+                if (ImGui::MenuItem("Quit", "Alt-F4"))
+                    break;
+                ImGui::EndMenu();
+            }
+            if (ImGui::BeginMenu("Edit"))
+            {
+                bool ro = editor.IsReadOnly();
+                if (ImGui::MenuItem("Read-only mode", nullptr, &ro))
+                    editor.SetReadOnly(ro);
+                ImGui::Separator();
+
+                if (ImGui::MenuItem("Undo", "ALT-Backspace", nullptr, !ro && editor.CanUndo()))
+                    editor.Undo();
+                if (ImGui::MenuItem("Redo", "Ctrl-Y", nullptr, !ro && editor.CanRedo()))
+                    editor.Redo();
+
+                ImGui::Separator();
+
+                if (ImGui::MenuItem("Copy", "Ctrl-C", nullptr, editor.HasSelection()))
+                    editor.Copy();
+                if (ImGui::MenuItem("Cut", "Ctrl-X", nullptr, !ro && editor.HasSelection()))
+                    editor.Cut();
+                if (ImGui::MenuItem("Delete", "Del", nullptr, !ro && editor.HasSelection()))
+                    editor.Delete();
+                if (ImGui::MenuItem("Paste", "Ctrl-V", nullptr, !ro && ImGui::GetClipboardText() != nullptr))
+                    editor.Paste();
+
+                ImGui::Separator();
+
+                if (ImGui::MenuItem("Select all", nullptr, nullptr))
+                    editor.SetSelection(TextEditor::Coordinates(), TextEditor::Coordinates(editor.GetTotalLines(), 0));
+
+                ImGui::EndMenu();
+            }
+
+            if (ImGui::BeginMenu("View"))
+            {
+                if (ImGui::MenuItem("Dark palette"))
+                    editor.SetPalette(TextEditor::GetDarkPalette());
+                if (ImGui::MenuItem("Light palette"))
+                    editor.SetPalette(TextEditor::GetLightPalette());
+                if (ImGui::MenuItem("Retro blue palette"))
+                    editor.SetPalette(TextEditor::GetRetroBluePalette());
+                ImGui::EndMenu();
+            }
+            ImGui::EndMenuBar();
+        }
+        ImGui::Text("%6d/%-6d %6d lines  | %s | %s | %s | %s", cpos.mLine + 1, cpos.mColumn + 1, editor.GetTotalLines(),
+            editor.IsOverwrite() ? "Ovr" : "Ins",
+            editor.CanUndo() ? "*" : " ",
+            editor.GetLanguageDefinition().mName.c_str(), (workPath + std::string("\\1691C.cpp")).c_str());
+
+        editor.Render("TextEditor");
+        ImGui::End();
 
         if (fileDialog.HasSelected())
         {
             workPath = fileDialog.GetSelected().string();
+            fileDialog.Close();
         }
 
         ImGui::Render();
