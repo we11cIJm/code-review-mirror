29c7efb8416fa495d4f405890159049f79d5438b
WeCanStart <113310973+WeCanStart@users.noreply.github.com>
2023-11-15 15:14:25 +0300

    Sync with own repo

--- app.cpp
+++ app.cpp
@@ -9,7 +9,7 @@
 #include <stdio.h>
 
 #include "imgui-filebrowser/imfilebrowser.h"
-#include "imGuiColorTextEdit/TextEditor.h"
+#include "ImGuiColorTextEdit/TextEditor.h"
 
 #define GL_SILENCE_DEPRECATION
 #if defined(IMGUI_IMPL_OPENGL_ES2)
@@ -131,16 +131,6 @@ int main(int, char**)
         int x, y;
         glfwGetWindowSize(window, &x, &y);
         //glfwSetFont();
-        {
-            ImGui::SetNextWindowPos(ImVec2(0, 0), 0, ImVec2(0, 0));
-            ImGui::SetNextWindowSize(ImVec2((float)(x) / 2, y), 0);
-
-            ImGui::Begin("Text");
-            ImGui::Text(std::filesystem::current_path().string().data());
-            ImGui::SetWindowFontScale((float)(x) / 1920);
-            ImGui::Text("artem");
-            ImGui::End();
-        }
 
         {
             //glfwSetFont();
@@ -189,12 +179,18 @@ int main(int, char**)
             fileDialog.Display();
             
             if (workPath != readPath) {
-                readPath = workPath;
-                std::ifstream t(readPath + std::string("\\1691C.cpp"), std::ios_base::in | std::ios_base::out);
-                if (t.good())
-                {
-                    std::string str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
-                    editor.SetText(str);
+                if (!std::filesystem::exists(workPath + std::string("\\1691C.cpp"))) {
+                    workPath = readPath;
+                    
+                }
+                else {
+                    readPath = workPath;
+                    std::ifstream t(readPath + std::string("\\1691C.cpp"), std::ios_base::in | std::ios_base::out);
+                    if (t.good())
+                    {
+                        std::string str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+                        editor.SetText(str);
+                    }
                 }
             }
             
@@ -214,72 +210,78 @@ int main(int, char**)
 
         auto cpos = editor.GetCursorPosition();
 
-        ImGui::Begin("Text Editor Demo", nullptr, ImGuiWindowFlags_HorizontalScrollbar | ImGuiWindowFlags_MenuBar);
-        ImGui::SetWindowFontScale((float)(x) / 1920);
-        ImGui::SetWindowSize(ImVec2(800, 600), ImGuiCond_FirstUseEver);
-        if (ImGui::BeginMenuBar())
-        {
-            if (ImGui::BeginMenu("File"))
+        ImGui::SetNextWindowPos(ImVec2(0, 0), 0, ImVec2(0, 0));
+        ImGui::SetNextWindowSize(ImVec2((float)(x) / 2, y), 0);
+
+        if (workPath != "") {
+            ImGui::Begin("Text Editor Demo", nullptr, ImGuiWindowFlags_HorizontalScrollbar | ImGuiWindowFlags_MenuBar);
+            ImGui::SetWindowFontScale((float)(x) / 1920);
+            /*ImGui::SetWindowSize(ImVec2(800, 600), ImGuiCond_FirstUseEver); */
+            if (ImGui::BeginMenuBar())
             {
-                if (ImGui::MenuItem("Save"))
+                if (ImGui::BeginMenu("File"))
                 {
-                    auto textToSave = editor.GetText();
-                    /// save text....
+                    /*if (ImGui::MenuItem("Save"))
+                    {
+                        auto textToSave = editor.GetText();
+                        /// save text....
+                    }*/
+                    if (ImGui::MenuItem("Quit", "Alt-F4"))
+                        break;
+                    ImGui::EndMenu();
                 }
-                if (ImGui::MenuItem("Quit", "Alt-F4"))
-                    break;
-                ImGui::EndMenu();
-            }
-            if (ImGui::BeginMenu("Edit"))
-            {
-                bool ro = editor.IsReadOnly();
-                if (ImGui::MenuItem("Read-only mode", nullptr, &ro))
+                if (ImGui::BeginMenu("Edit"))
+                {
+                    bool ro = editor.IsReadOnly();
                     editor.SetReadOnly(ro);
-                ImGui::Separator();
+                    /*if (ImGui::MenuItem("Read-only mode", nullptr, &ro))
+                        editor.SetReadOnly(ro);
+                    ImGui::Separator();*/
 
-                if (ImGui::MenuItem("Undo", "ALT-Backspace", nullptr, !ro && editor.CanUndo()))
-                    editor.Undo();
-                if (ImGui::MenuItem("Redo", "Ctrl-Y", nullptr, !ro && editor.CanRedo()))
-                    editor.Redo();
+                    /*if (ImGui::MenuItem("Undo", "ALT-Backspace", nullptr, !ro && editor.CanUndo()))
+                        editor.Undo();
+                    if (ImGui::MenuItem("Redo", "Ctrl-Y", nullptr, !ro && editor.CanRedo()))
+                        editor.Redo();
 
-                ImGui::Separator();
+                    ImGui::Separator();*/
 
-                if (ImGui::MenuItem("Copy", "Ctrl-C", nullptr, editor.HasSelection()))
-                    editor.Copy();
-                if (ImGui::MenuItem("Cut", "Ctrl-X", nullptr, !ro && editor.HasSelection()))
-                    editor.Cut();
-                if (ImGui::MenuItem("Delete", "Del", nullptr, !ro && editor.HasSelection()))
-                    editor.Delete();
-                if (ImGui::MenuItem("Paste", "Ctrl-V", nullptr, !ro && ImGui::GetClipboardText() != nullptr))
-                    editor.Paste();
+                    if (ImGui::MenuItem("Copy", "Ctrl-C", nullptr, editor.HasSelection()))
+                        editor.Copy();
+                    /*if (ImGui::MenuItem("Cut", "Ctrl-X", nullptr, !ro && editor.HasSelection()))
+                        editor.Cut();
+                    if (ImGui::MenuItem("Delete", "Del", nullptr, !ro && editor.HasSelection()))
+                        editor.Delete();
+                    if (ImGui::MenuItem("Paste", "Ctrl-V", nullptr, !ro && ImGui::GetClipboardText() != nullptr))
+                        editor.Paste();*/
 
-                ImGui::Separator();
+                    ImGui::Separator();
 
-                if (ImGui::MenuItem("Select all", nullptr, nullptr))
-                    editor.SetSelection(TextEditor::Coordinates(), TextEditor::Coordinates(editor.GetTotalLines(), 0));
+                    if (ImGui::MenuItem("Select all", nullptr, nullptr))
+                        editor.SetSelection(TextEditor::Coordinates(), TextEditor::Coordinates(editor.GetTotalLines(), 0));
 
-                ImGui::EndMenu();
+                    ImGui::EndMenu();
+                }
+
+                if (ImGui::BeginMenu("View"))
+                {
+                    if (ImGui::MenuItem("Dark palette"))
+                        editor.SetPalette(TextEditor::GetDarkPalette());
+                    if (ImGui::MenuItem("Light palette"))
+                        editor.SetPalette(TextEditor::GetLightPalette());
+                    if (ImGui::MenuItem("Retro blue palette"))
+                        editor.SetPalette(TextEditor::GetRetroBluePalette());
+                    ImGui::EndMenu();
+                }
+                ImGui::EndMenuBar();
             }
+            ImGui::Text("%6d/%-6d %6d lines  | %s | %s | %s | %s", cpos.mLine + 1, cpos.mColumn + 1, editor.GetTotalLines(),
+                editor.IsOverwrite() ? "Ovr" : "Ins",
+                editor.CanUndo() ? "*" : " ",
+                editor.GetLanguageDefinition().mName.c_str(), (workPath + std::string("\\1691C.cpp")).c_str());
 
-            if (ImGui::BeginMenu("View"))
-            {
-                if (ImGui::MenuItem("Dark palette"))
-                    editor.SetPalette(TextEditor::GetDarkPalette());
-                if (ImGui::MenuItem("Light palette"))
-                    editor.SetPalette(TextEditor::GetLightPalette());
-                if (ImGui::MenuItem("Retro blue palette"))
-                    editor.SetPalette(TextEditor::GetRetroBluePalette());
-                ImGui::EndMenu();
-            }
-            ImGui::EndMenuBar();
+            editor.Render("TextEditor");
+            ImGui::End();
         }
-        ImGui::Text("%6d/%-6d %6d lines  | %s | %s | %s | %s", cpos.mLine + 1, cpos.mColumn + 1, editor.GetTotalLines(),
-            editor.IsOverwrite() ? "Ovr" : "Ins",
-            editor.CanUndo() ? "*" : " ",
-            editor.GetLanguageDefinition().mName.c_str(), (workPath + std::string("\\1691C.cpp")).c_str());
-
-        editor.Render("TextEditor");
-        ImGui::End();
 
         if (fileDialog.HasSelected())
         {
